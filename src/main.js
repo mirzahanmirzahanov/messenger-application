4b79d31c329504c1f0838bcd32bd087a2ec7ed7a
mirzahanmirzahanov <www.mirzahanmirzahanov@gmail.com>
2022-07-18 00:57:54 +0300

    init commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'material-design-icons-iconfont'
+import './assets/styles/scss/styles.scss'
 
 createApp(App).use(store).use(router).mount('#app')
